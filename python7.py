593979c90981fe0bb6764372366687598a47b271
Daniil <daniil.kobzar299@gmail.com>
2022-03-21 18:19:14 +0100

    Task 7 added

--- python7.py
+++ python7.py
@@ -1,6 +1,16 @@
 # Проверить истинность утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат
-import random
-x = random.randint(0,2)
-y = random.randint(0,2)
-z = random.randint(0,2)
-print((x and y and z) == !x or !y or !Z)
+x = False
+y = True
+Z = False
+
+for x in range(2):
+    for y in range(2):
+        for Z in range(2):
+            r1 = not (x or y or Z)
+            r2 = not x and not y and not Z
+            
+
+if r1 == r2:
+    print("True, Ley de Morgan ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z")
+else:
+    print("False, ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z")
