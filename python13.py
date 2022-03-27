290f439acc5b0c571f4d5226fc0b703be687bf32
Daniil <daniil.kobzar299@gmail.com>
2022-03-27 19:02:29 +0200

    Task 12 modifed and added

---
+++ python13.py
@@ -0,0 +1,6 @@
+# Пользователь задаёт две строки. Определить количество вхождений одной строки в другой.
+str = input("Enter first message: ")
+str2 = input("Enter second message: ")
+
+print(str2.count(str))
+print(str.count(str2))
