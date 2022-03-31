96d313df16ccd79d017d8050c00a5a8cca38c10c
Daniil <daniil.kobzar299@gmail.com>
2022-03-31 16:58:21 +0200

    Task 27 modifed

--- python27.py
+++ python27.py
@@ -1,20 +1,14 @@
 # Строка содержит набор чисел. Показать большее и меньшее число
-import random
-list = [random.randint(1, 101),random.randint(1, 101), random.randint(1, 101), random.randint(1, 101) ]
-listlenght = len(list)
-max = 0
-min = list[0]
-for i in range(listlenght):
-    if list[i] > max:
-        max = list[i]
+string = (input('Write some numbers with space between them: ').split())
+maxnum = max(string)
+minum = min(string)
+print(string)
+print(maxnum)
+print(minum)
 
-for i in range(listlenght):
-    if list[i] < min:
-        min = list[i]
 
-print(list)
-print(max)
-print(min)    
+
+
 
 
     
